6b06f298ef0d28c27d41d37312b518ac2d07d96a
rednerv <rednerv@163.com>
2024-10-20 17:31:35 +0800

    customize.sh

--- customize.sh
+++ customize.sh
@@ -11,5 +11,5 @@ sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-#3. Replace with JerryKuKu’s Argon
-#rm openwrt/package/lean/luci-theme-argon -rf
+#3. Replace with kucat
+rm openwrt/package/lean/luci-theme-kucat -rf
